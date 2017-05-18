17ce2f6e799e5fecf6c8aff0215e2f04dfaa3b6d
April Webster <awebster@us.ibm.com>
2017-05-18 10:17:24 -0700

    added onVote function to props and added onVote to the ConversationItem

--- views/output.jsx
+++ views/output.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import ConversationItem from './conversation-item.jsx';
 
-export default function Output(props) {
-  return (
-    <div className="conversation">
-      <div className="conversation_header">
-        <div className="statement_header">Conversation</div>
-        <div className="tone_header"> Tone Analysis</div>
-      </div>
+const Output = React.createClass({
 
-      {/* Generate a div for each utterance in the conversation object */}
-      <div>
-        { props.conversation.map((utterance, i) =>
-          <ConversationItem key={utterance.statement.text} utterance={utterance} utterance_id={i} />,
-        )
-      }
-      </div>
-    </div>
-  );
-}
+  displayName: 'output',
 
-Output.propTypes = {
-  //eslint-disable-next-line
-  conversation: React.PropTypes.array.isRequired,
-};
+  propTypes: {
+    conversation: React.PropTypes.array.isRequired,
+    onVote: React.PropTypes.func.isRequired,
+  },
+
+  getDefaultProps() {
+    return {
+      //eslint-disable-next-line
+      conversation: null,
+      onVote(curInput) { console.log('onVote '.concat(curInput)); },
+    };
+  },
+
+  render() {
+    return (
+      <div className="conversation">
+        <div className="conversation_header">
+          <div className="statement_header">Conversation</div>
+          <div className="tone_header"> Tone Analysis</div>
+        </div>
+
+        {/* Generate a div for each utterance in the conversation object */}
+        <div>
+          {
+            this.props.conversation.map((utterance, i) =>
+              <ConversationItem
+                key={utterance.statement.text}
+                utterance={utterance}
+                utterance_id={i}
+                onVote={this.props.onVote}
+              />,
+          )
+        }
+        </div>
+      </div>
+    );
+  },
+
+});
+
+export default Output;

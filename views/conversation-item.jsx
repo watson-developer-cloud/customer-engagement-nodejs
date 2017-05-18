5502a15e113fc8fe22a7bf0bf1a686acb5c8a318
Alex Meyer <alexmeyer@us.ibm.com>
2017-05-18 16:05:29 -0500

    added link and container
    you will need to add the form buttons and display functionality
    also fixed the voting icons to have a pointer

--- views/conversation-item.jsx
+++ views/conversation-item.jsx
@@ -49,6 +49,7 @@ const ConversationItem = React.createClass({
     const tones = this.props.utterance.tones;
 
     return (
+      <div>
       <div className={user.type === 'customer' ? 'speaker consumer' : 'speaker'}>
         <div className="avatar">
           <div className={user.type === 'customer' ? 'customer_avatar' : 'agent_avatar'} />
@@ -80,17 +81,22 @@ const ConversationItem = React.createClass({
                   buttons={[{
                     value: 1,
                     id: 'utterance'.concat('-', this.props.utterance_id, '-', i, '-', t.tone, '-true'),
-                    text: <Icon type={'thumbs-up'} fill={Colors.gray_30} />,
+                    text: <Icon className="thumb" type={'thumbs-up'} fill={Colors.gray_30} />,
                   }, {
                     value: 0,
                     id: 'utterance'.concat('-', this.props.utterance_id, '-', i, '-', t.tone, '-false'),
-                    text: <Icon type={'thumbs-down'} fill={Colors.gray_30} />,
+                    text: <Icon className="thumb" type={'thumbs-down'} fill={Colors.gray_30} />,
                   }]}
                 />
               </div>
             ))}
+            <div className="other_tones"><a className="base--a jumbotron--nav-link" href="#">What other tones...?</a></div>
         </div>
       </div>
+      <div className="speaker hidden">
+        <span className="description"> What other tones do you think are in this staement?</span>
+      </div>
+    </div>
     );
   },
 });

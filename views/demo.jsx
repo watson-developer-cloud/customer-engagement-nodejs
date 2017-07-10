a41057f98f84f269ab39c06840958a6890bd7c75
April Webster <awebster@us.ibm.com>
2017-07-09 23:21:18 -0700

    added in an initializing flag and componentDidMount functions to support a dynamic call to Tone Analyzer to get the data to initialize the conversation state as opposed to using a static file.  Updated resetConversation to also reset and close any open, populated alternate tones data collection components (checkboxes) - required moving votes for alternative tones state to the demo component.

--- views/demo.jsx
+++ views/demo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { JsonLinkInline } from 'watson-react-components';
+import fetch from 'isomorphic-fetch';
+import { JsonLinkInline, Icon } from 'watson-react-components';
 import Output from './output.jsx';
 import Input from './input.jsx';
 import ResetConversationLink from './reset-conversation-link.jsx';
@@ -8,14 +9,24 @@ import ResetConversationLink from './reset-conversation-link.jsx';
 // hack to get around deep clone of initial conversation for reseting conversation
 const initialConversationString = JSON.stringify(require('../public/data/initial-conversation'));
 const initialConversation = require('../public/data/initial-conversation');
+const systemConversation = require('../public/data/initial-tone-analyzer-payload');
+
+const CUSTOMER_NAME = 'Fred';
+const CUSTOMER_HANDLE = '@Fred_theConsumer';
+const AGENT_NAME = 'Agent';
+const AGENT_HANDLE = '@Agent_technology_company';
+
 const MAX_TONES_TO_DISPLAY = 4;
+const MAX_LENGTH_USER_UTTERANCE = 500;
 
 const Demo = React.createClass({
   displayName: 'Conversation',
 
   /**
   * Method to initialize the state of the Conversation react component (ES5 compatible)
-  * The initial state for the conversation is stored in a json file
+  * The initial state for the conversation is stored in a json file - this is a hack
+  * as the component won't wait for the response payload from an async call to Tone Analyzer
+  * prior to rendering.
   */
   getInitialState() {
     const initialLastUtterance = initialConversation.utterances[initialConversation.utterances.length - 1];
@@ -26,7 +37,28 @@ const Demo = React.createClass({
       newUtteranceAvatarType: initialLastUtterance.user.type === 'agent' ? 'customer_avatar' : 'agent_avatar', // 'customer_avatar'
       showJson: false,
       isResetting: false,
+      loading: false,
+      initializing: true,
     };
+  },
+
+  componentDidMount() {
+    fetch('/api/tone_chat', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(systemConversation),
+    }).then(this.handleErrors).then((response) => {
+      response.json().then((tone) => {
+        this.setState({
+          conversation: this.createConversationJson(tone, 'agent'),
+          initializing: false,
+        });
+      });
+    }).catch((error) => {
+      this.setState({
+        error,
+      });
+    });
   },
 
   onShowJson() {
@@ -42,38 +74,73 @@ const Demo = React.createClass({
   },
 
   onSubmit(utterance) {
-    fetch('/api/tone_chat', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        utterances: [
-          { text: utterance, user: 'customer' },
-        ],
-      }),
-    }).then(this.handleErrors).then((response) => {
-      response.json().then((tone) => {
-        this.updateConversation(tone);
-      });
-    }).catch((error) => {
-      this.setState({
-        error,
-        //loading: false,
-      });
+    this.setState({
+      loading: true,
+      error: null,
     });
+
+    // Tone Analyzer tone_chat API endpoint has a max utterance size of 500 characters
+    if (utterance.length > MAX_LENGTH_USER_UTTERANCE) {
+      this.setState({
+        error: {
+          message: 'Utterance must be '.concat(MAX_LENGTH_USER_UTTERANCE).concat(' characters or shorter.  Please try again.'),
+        },
+        loading: false,
+      });
+    } else {
+      fetch('/api/tone_chat', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({
+          utterances: [
+            { text: utterance, user: 'customer' },
+          ],
+        }),
+      }).then(this.handleErrors).then((response) => {
+        response.json().then((tone) => {
+          this.updateConversation(tone);
+          this.setState({
+            loading: false,
+          });
+        });
+      }).catch((error) => {
+        this.setState({
+          error,
+          loading: false,
+        });
+      });
+    }
   },
 
-  onVote(voteData, source) {
-    if (source === 'user') {
+  onVote(utterance, tone, vote) {
+    const updatedConversation = this.state.conversation;
+    const currentUtterance = updatedConversation.utterances.filter(u => u.id === utterance.id);
+    const updatedUtteranceVotes = utterance.utterance_votes;
+    updatedUtteranceVotes[tone] = vote;
+    currentUtterance.utterances_votes = updatedUtteranceVotes;
+    this.setState({
+      conversation: updatedConversation,
+    });
+
+    const voteData = {
+      statement: utterance.statement.text,
+      user_feedback: {
+        tone,
+        vote,
+      },
+      tone_analyzer_payload: utterance.tone_analyzer_payload,
+    };
+
+    if (utterance.source === 'user') {
       fetch('/log_perceived_accuracy', {
         method: 'POST',
         headers: { 'content-type': 'application/json' },
         body: JSON.stringify(voteData),
       }).then(this.handleErrors).then((response) => {
-          console.log('vote logged: '.concat(response));
+          console.log('watson tone accuracy logged: '.concat(response));
       }).catch((error) => {
         this.setState({
           error,
-          //loading: false,
         });
       });
     }
@@ -85,11 +152,10 @@ const Demo = React.createClass({
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(newToneData),
     }).then(this.handleErrors).then((response) => {
-        console.log('new tone logged: '.concat(response));
+        console.log('suggested tone logged: '.concat(response));
     }).catch((error) => {
       this.setState({
         error,
-        //loading: false,
       });
     });
   },
@@ -101,39 +167,58 @@ const Demo = React.createClass({
     return response;
   },
 
-  /**
-  * Create a new conversation object to be added to the conversation state.
-  * The toneAnalyzerPayload is parsed and mapped into a format usable by the
-  * front-end UI.
+  /*
+    utteranceId: id for the utterance for which the conversation turn is created
+    customerTonePayloadObject: response payload from the Tone Analyzer tone_chat endpoint
+    speaker: one of two strings - 'agent' or 'customer' indicating who made the statement/utterance
+    source: one of two strings - 'system' or 'user'.  This is to distinguish between system provided utterances for the conversation initialization
+      and utterances provided by the user through the input text box in the UI.
+    timestamp: 'now' or (tonesShortlist.length - i).toString().concat(' min ago') for user-generated timestamps
   */
-  updateConversation(toneAnalyzerPayload) {
-    const tonesRaw = toneAnalyzerPayload.utterances_tone[0].tones;
-
-    // sort raw tones in descending order
+  createConversationTurn(utteranceId, customerTonePayloadObject, speaker, source, timestamp) {
+    // sort and map analyzer tones to a json object
+    const tonesRaw = customerTonePayloadObject.tones;
     tonesRaw.sort((tone1, tone2) =>
       parseFloat(tone2.score) - parseFloat(tone1.score),
     );
+    const sortedTones = tonesRaw.map(t => ({ tone: t.tone_name, score: t.score.toFixed(2) }));
+    const tones = sortedTones.slice(0, MAX_TONES_TO_DISPLAY);
 
-    // map each raw tone to the required json object
-    const tones = tonesRaw.map(t => ({ tone: t.tone_name, score: t.score }));
-    const tonesShortlist = tones.slice(0, MAX_TONES_TO_DISPLAY);
-
-    // create new conversation turn json object to add to the conversation state
-    const lastConversationTurn = this.state.conversation.utterances[this.state.conversation.utterances.length - 1];
-    const newConversationTurn = {
-      source: 'user',
+    // create new conversation turn object for rendering purposes
+    const conversationTurn = {
+      utteranceId,
+      source,
       user: {
-        type: lastConversationTurn.user.type === 'agent' ? 'customer' : 'agent',
-        name: lastConversationTurn.user.type === 'agent' ? this.state.conversation.customer.name : this.state.conversation.agent.name,
-        handle: lastConversationTurn.user.type === 'agent' ? this.state.conversation.customer.handle : this.state.conversation.agent.handle,
+        type: speaker,
+        name: speaker === 'agent' ? AGENT_NAME : CUSTOMER_NAME,
+        handle: speaker === 'agent' ? AGENT_HANDLE : CUSTOMER_HANDLE,
       },
       statement: {
-        text: toneAnalyzerPayload.utterances_tone[0].utterance_text,
-        timestamp: 'now',
+        text: customerTonePayloadObject.utterance_text,
+        timestamp,
       },
-      tones: tonesShortlist,
-      tone_analyzer_payload: toneAnalyzerPayload,
+      tones,
+      tone_analyzer_payload: customerTonePayloadObject,
+      utterance_votes: {},
     };
+
+    return conversationTurn;
+  },
+
+  /**
+  * Create a new conversation object to be added to the conversation state.
+  * The toneAnalyzerPayload is parsed and mapped into a format used by the
+  * front-end UI.
+  */
+  updateConversation(toneAnalyzerPayload) {
+    const lastConversationTurn = this.state.conversation.utterances[this.state.conversation.utterances.length - 1];
+    const newConversationTurn = this.createConversationTurn(
+      lastConversationTurn.id + 1,
+      toneAnalyzerPayload.utterances_tone[0],
+      lastConversationTurn.user.type === 'agent' ? 'customer' : 'agent',
+      'user',
+      'now',
+    );
 
     // push new conversation turn to the conversation state and setState
     const conversation = this.state.conversation;
@@ -147,47 +232,98 @@ const Demo = React.createClass({
     });
   },
 
+  createConversationJson(payload, firstTurnType) {
+    let lastConversationTurnSpeaker = firstTurnType;
+    const conversationJson = {
+      customer: {
+        name: 'Fred',
+        handle: '@Fred_theConsumer',
+      },
+      agent: {
+        name: 'Agent',
+        handle: '@Agent_technology_company',
+      },
+      utterances: [],
+    };
+
+    const listOfTones = payload.utterances_tone;
+
+    for (let i = 0, len = listOfTones.length; i < len; i += 1) {
+      const conversationTurn = this.createConversationTurn(i, payload.utterances_tone[i],
+        lastConversationTurnSpeaker === 'agent' ? 'customer' : 'agent',
+        'system',
+        (listOfTones.length - i).toString().concat(' min ago'),
+      );
+
+      conversationJson.utterances.push(conversationTurn);
+      lastConversationTurnSpeaker = conversationTurn.user.type;
+    }
+    return conversationJson;
+  },
+
   resetConversation() {
-    this.setState({
-      conversation: JSON.parse(initialConversationString),
-      error: null,
-      newUtterancePlaceholder: JSON.parse(initialConversationString).agent.handle,
-      newUtteranceAvatarType: 'customer_avatar',
-      isResetting: true,
+    fetch('/api/tone_chat', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(systemConversation),
+    }).then(this.handleErrors).then((response) => {
+      response.json().then((tone) => {
+        this.setState({
+          conversation: this.createConversationJson(tone, 'agent'),
+          error: null,
+          newUtterancePlaceholder: JSON.parse(initialConversationString).agent.handle,
+          newUtteranceAvatarType: 'customer_avatar',
+          isResetting: true,
+        });
+        setTimeout(() => {
+          this.setState({ isResetting: false });
+        }, 1);
+      });
+    }).catch((error) => {
+      this.setState({
+        error,
+      });
     });
-    setTimeout(() => {
-      this.setState({ isResetting: false });
-    }, 1);
   },
 
   render() {
     return (
-      <div>
-        <Output
-          conversation={this.state.conversation.utterances}
-          onVote={this.onVote}
-          onRecordOtherTone={this.onRecordOtherTone}
-          isResetting={this.state.isResetting}
-        />
-        <Input
-          error={this.state.error}
-          onSubmit={this.onSubmit}
-          newUtterancePlaceholder={this.state.newUtterancePlaceholder}
-          newUtteranceAvatarType={this.state.newUtteranceAvatarType}
-        />
-
-        <div className="conversation_footer">
-          <JsonLinkInline
-            json={this.state.conversation}
-            showJson={this.state.showJson}
-            onExit={this.onExitJson}
-            onShow={this.onShowJson}
-            description={
-              <ResetConversationLink resetConversation={this.resetConversation} />
+      this.state.initializing ?
+        (
+          <div className="loading_container">
+            <Icon type="loader" size="large" />
+          </div>) :
+          (<div>
+            <Output
+              conversation={this.state.conversation.utterances}
+              onVote={this.onVote}
+              onRecordOtherTone={this.onRecordOtherTone}
+              isResetting={this.state.isResetting}
+            />
+            { this.state.loading ?
+              (
+                <div className="loading_container">
+                  <Icon type="loader" size="large" />
+                </div>) :
+                <Input
+                  error={this.state.error}
+                  onSubmit={this.onSubmit}
+                  newUtterancePlaceholder={this.state.newUtterancePlaceholder}
+                  newUtteranceAvatarType={this.state.newUtteranceAvatarType}
+                />
             }
-          />
-        </div>
-      </div>
+            <div className="conversation_footer">
+              <JsonLinkInline
+                json={this.state.conversation}
+                showJson={this.state.showJson}
+                onExit={this.onExitJson}
+                onShow={this.onShowJson}
+                description={
+                  <ResetConversationLink resetConversation={this.resetConversation} />
+                }
+              />
+            </div>
+          </div>)
     );
   },
 

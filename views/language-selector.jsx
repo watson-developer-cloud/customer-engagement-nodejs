79e2fcd038a10e48839416802546f667b7aeda69
Richie Verma <richie.v@ibm.com>
2018-03-09 09:02:03 -0800

    Converted to pure stateless function and added currentLanguage prop that is used to set the current language button.

--- views/language-selector.jsx
+++ views/language-selector.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
 import { ButtonsGroup } from 'watson-react-components';
 
-const LanguageSelector = React.createClass({
+function LanguageSelector(props) {
+  let englishSelected = false;
 
-  displayName: 'LanguageSelector',
-
-  propTypes: {
-    onLanguageSelection: React.PropTypes.func.isRequired,
-  },
-
-  render() {
-    return (
-      <div className="language_selection_container">
-        <div className="select_language_header">Select Language: </div>
-        <div className="language_options">
-          {
-            <ButtonsGroup
-              type="radio"
-              name="radio-buttons"
-              onClick={e => this.props.onLanguageSelection(e.target.id)}
-              buttons={[{
-                value: 1,
-                id: 'en',
-                text: 'English',
-              }, {
-                value: 2,
-                id: 'fr',
-                text: 'French',
-              }]}
-            />
-        }
-        </div>
+  if (props.currentLanguage === 'en') {
+    englishSelected = true;
+  }
+  // boo
+  return (
+    <div className="language_selection_container">
+      <div className="select_language_header">Select Language: </div>
+      <div className="language_options">
+        {
+          <ButtonsGroup
+            type="radio"
+            name="radio-buttons"
+            onClick={(e) => {
+              props.onLanguageSelection(e.target.id);
+            }}
+            buttons={[{
+              value: 1,
+              id: 'en',
+              text: 'English',
+              selected: englishSelected,
+            }, {
+              value: 2,
+              id: 'fr',
+              text: 'French',
+              selected: !englishSelected,
+            }]}
+          />
+      }
       </div>
-    );
-  },
+    </div>
+  );
+}
 
-});
+LanguageSelector.propTypes = {
+  onLanguageSelection: React.PropTypes.func.isRequired,
+  currentLanguage: React.PropTypes.string.isRequired,
+};
 
 export default LanguageSelector;

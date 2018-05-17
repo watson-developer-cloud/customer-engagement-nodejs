a88544a00a2bd3df072ba4fdd82f8d9eed9a16ec
Richie Verma <richie.v@ibm.com>
2018-05-17 15:00:23 -0700

    Update analytics

--- views/layout.jsx
+++ views/layout.jsx
@@ -40,7 +40,7 @@ function Layout(props) {
           {props.children}
         </div>
         <script type="text/javascript" src="js/bundle.js" />
-        <script type="text/javascript" src="js/ga.js" />
+        <script type="text/javascript" src="js/analytics.js" />
       </body>
     </html>
   );
